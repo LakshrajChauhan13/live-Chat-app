0d9f67d2894433dfc031fa61d7d6b9884f6d9e57
LakshrajChauhan13 <lakshrajchauhan13@gmail.com>
2026-03-12 22:22:57 +0530

    feat(fe): add 'delete popover'

--- CHAT-APP-FE/src/components/MessageBox.tsx
+++ CHAT-APP-FE/src/components/MessageBox.tsx
@@ -1,43 +1,92 @@
 import { cn } from "../lib/utils"
+import Vertical_ellipsisIcon from '../icons/Vertical_ellipsisIcon'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { useState } from "react";
+import type { messageArrayInterface } from "./pages/ChatRoomPage";
 
 interface MessageInterface {
-    message: string;
+    message: messageArrayInterface;
     key: number;
-    userId: string
+    userId: string;
+    id: string;
+    deleteMessage: (messageId: string) => void;
 }
 
-const MessageBox = ({message,userId, key}: MessageInterface) => {        //receiver
+const MessageBox = ({message, userId, key, id, deleteMessage}: MessageInterface) => {        //receiver
+    function onDelete(){
+        deleteMessage(id)
+    }
+
   return (
       <div className=" flex flex-col gap-1.5 shrink-0  w-full ">
-        <ChatMessage key={key} message={message} userId={userId} className3="flex" className2="ml-1 rounded-tr-lg bg-neutral-950" className1="bg-green-500" />
-          {/* <div className="size-8 bg-yellow-500 rounded-full" />
-          <div className="w-[40%] min-h-8 max-h-full ml-1 text-sm p-1 pl-2 bg-neutral-950 text-white  rounded-b-lg ">
-            {message}
-          </div> */}
+        <ChatMessage key={key} message={message} userId={userId} deleteMessage={onDelete} messageId={id}  
+        className3="flex" 
+        className2={`ml-1 rounded-tr-lg  `} 
+        className1="bg-green-500" />
       </div>
   )
 }
 export default MessageBox
 
 
-export const MessageBox2 = ({message,userId, key}: MessageInterface) => {        //sender
+export const MessageBox2 = ({message, userId, key, id, deleteMessage}: MessageInterface) => {        //sender
+    function onDelete(){
+        deleteMessage(id)
+    }
+
     return (
-      <div className=" flex flex-col items-end gap-1.5 shrink-0  w-full">
-         <ChatMessage key={key} message={message} userId={userId} className3=" flex flex-row-reverse" className2="mr-1 rounded-tl-lg bg-neutral-950 " />
+      <div className=" flex flex-col items-end gap-1.5 shrink-0 w-full  ">
+        <ChatMessage key={key} message={message} userId={userId} deleteMessage={onDelete} messageId={id} 
+        className3="flex flex-row-reverse" 
+        className2={`mr-1 rounded-tl-lg `} 
+        className1="bg-amber-500"/>
       </div>
     )
 }
 
-const ChatMessage = ({className1, className2, className3, message, userId}: {className1?: string, message: string, className3?: string, className2?: string, userId: string}) => {
+const ChatMessage = ({className1, className2, className3, message, userId, deleteMessage, messageId}: {
+    className1?: string, 
+    className2?: string, 
+    className3?: string, 
+    message: messageArrayInterface, 
+    userId: string, 
+    deleteMessage: () => void,
+    messageId: string
+}) => {
+    const [isVisible, setIsVisible] = useState(false)
+    
+    function visible() {
+        setIsVisible(c => !c);    
+    }
+
+    const isDeleted = message.message === 'This message has been deleted.';
     return (
-        <>
-            <div className={cn( "flex items-center gap-1", className3)}>
-                <div className={cn("size-8 bg-amber-500 rounded-full", className1)} />
-                <span className="text-xs px-1 py-1 rounded-lg shadow-aceternity"> {userId}  </span>
+        <div className="flex flex-col gap-2 group">
+            <div className={cn( "flex items-center gap-1 " , className3)}>
+                <div className={cn("size-8 bg-red-500 rounded-full", className1)} />
+                <span className="text-xs px-1 py-1 rounded-lg shadow-aceternity-dark text-neutral-500"> 
+                    {userId}  
+                </span>
+                <Popover>
+                    <PopoverTrigger asChild>
+                        <button onClick={visible} className={`group-hover:visible invisible ${isVisible ? 'visible' : ''} cursor-pointer ${isDeleted && 'hidden'}`}>
+                            {userId === "You" && <Vertical_ellipsisIcon /> }      
+                        </button>
+                    </PopoverTrigger>
+                    <PopoverContent onClick={deleteMessage} align="end" className="w-20 h-10">
+                        Delete
+                    </PopoverContent>
+                </Popover>
             </div>
-            <div className={cn(" min-h-8 max-h-full max-w-100  wrap-break-word mr-1 text-sm p-1 pl-2 bg-amber-950/90 text-neutral-50  rounded-b-lg", className2)}>
-                {message}
+            <div className={cn(" min-h-8 max-h-full max-w-120 wrap-break-word mr-1 text-base font-mono inset-shadow-aceternity-dark shadow-aceternity-dark p-2 bg-amber-950/90 text-neutral-50 rounded-b-lg",
+                `${message.type === 'deleted' ? "italic bg-neutral-700 opacity-50" : 'bg-neutral-950'}`,
+                className2)}>
+                {message.message}
             </div>
-        </>
+        </div>
     )
 }   
