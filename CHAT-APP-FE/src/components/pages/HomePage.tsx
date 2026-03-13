4c59116468e39f9ce967b80a5de710945215e8f8
LakshrajChauhan13 <lakshrajchauhan13@gmail.com>
2026-03-13 21:32:09 +0530

    feat(fe): add homepage hero components and layout styles
    - Added new UI components: BlurText, DotGrid, and Heading for the hero section.
    - Refined entire UI
    - Refined global styles in index.css.
    - Refactored App.tsx to use TanStack Router Outlet and Sonner Toaster.

--- CHAT-APP-FE/src/components/pages/HomePage.tsx
+++ CHAT-APP-FE/src/components/pages/HomePage.tsx
@@ -1,34 +1,64 @@
-import { Link } from "@tanstack/react-router"
+import BlurText from "../BlurText"
 import ProjectTitle from "../brandName/ProjectTitle"
+import DotGrid from "../DotGrid"
+import Heading from "../ui/Heading"
+import LinkTag from "../ui/LinkTag"
 
 const HomePage = () => {
   return (
-    <div className="bg-neutral-950">
-        <div className='max-w-7xl mx-auto  h-screen flex flex-col gap-25 items-center '>
+    <div className="bg-neutral-950 min-h-screen relative overflow-hidden ">
+        <div className=" absolute inset-0 mask-x-from-60% ">
+            <DotGrid 
+                activeColor="#981651"
+                baseColor="#271E37"
+                dotSize={4}
+                gap={20}
+                className="opacity-100"
+            />
+        </div>
+        
+        <div className='max-w-7xl mx-auto flex flex-col gap-23.5 items-center justify-center relative z-10  '>
 
-            <main className="flex flex-col items-center">
-                <ProjectTitle  />
-                <h3 className="text-4xl font-sans-flex tracking-wider text-neutral-600">
-                    Fast. Secure. Private.
+            <main className="flex flex-col w-full text-center items-center ">
+                <ProjectTitle className="text-[250px] pt-12 -mt-25" />
+                <h3 className="text-5xl text-center leading-0 -mt-10 font-Bitcount-Prop-Double-Ink tracking-widest">
+                    <BlurText 
+                      text="Anonymous. Encrypted. Erasable."
+                      delay={500}
+                      easing={"circOut"}
+                    />
+                    <span className="opacity-40">
+                        Anonymous. Encrypted. Erasable.
+                    </span>
                 </h3>
             </main>
 
-            <p className=" text-2xl max-w-3xl text-center leading-normal text-neutral-500">
-                Private chat rooms, that will be vanished after all users left. No Accounts, no history.       
+            <p className="text-xl bg-neutral-500/10 px-10 py-5 pointer-events-none backdrop-blur-[2px] w-[90%] mask-x-from-85% max-w-6xl font-sans-flex text-center tracking-wider text-shadow-neutral-800 text-shadow-2xs font-thin text-pink-100 ">
+                Encrypted chat rooms that vanish the moment you leave.  
+                No Accounts, no logs, no history, just secure conversation.       
             </p>
             
 
-            <div className=" flex gap-5 font-sans-flex ">
-
-                <Link to={'/create'} className="text-xl border border-neutral-500 px-10 py-5  text-neutral-500">
+            <div className=" flex gap-5 font-sans-flex w-120 justify-center items-center relative max-w-5xl ">
+                <LinkTag to={'create'} className="h-full w-[45%] " >
                     Create Room
-                </Link>
+                </LinkTag>
 
-                <Link to={'/join'} className="text-xl px-11 py-5 border border-neutral-500 text-neutral-500">
+                <LinkTag to={'/join'} className="w-[45%]" >
                     Join Room
-                </Link>
+                </LinkTag>
             </div>
+            <div className="max-w-5xl  mx-auto space-y-5">
 
+                    <div className=" bg-linear-to-r from-transparent via-pink-500/50 to-transparent h-px w-full " />
+
+                    <div className=" flex font-sans-flex gap-5 w-full">
+                        <Heading heading="Live-Chat" title="Real-time delivery with zero latency via WebSockets." />
+                        <Heading heading="Auto-Deletion" title="Data is wiped from memory the moment the room closes." />
+                        <Heading heading="No Registration" title="Zero friction. No email or phone number required." />
+                    </div>
+            </div>
+          
         </div>
     </div>
 
---
+++ CHAT-APP-FE/src/components/ui/LinkTag.tsx
@@ -0,0 +1,17 @@
+import { cn } from '@/lib/utils'
+import { Link } from '@tanstack/react-router'
+import React from 'react'
+
+const LinkTag = ({to, className, children}: {to: string; className?: string; children: string}) => {
+  return (
+    <Link to={`${to}`} className={cn("text-lg py-6 border-2 h-full flex justify-center items-center", 
+    "border-neutral-600/50 text-neutral-400 overflow-hidden shadow-aceternity hover:-translate-y-0.5 active:translate-0 bg-bg shadow-white transition-all duration-200",
+      "after:content-[''] after:w-[90%] after:h-100 after:bg-neutral-600/10 backdrop-blur-2xl after:absolute after:inset-x-0 after:-bottom-10",
+                "after:rotate-22 after:-translate-x-10 hover:after:bg-neutral-200/10 after:-z-1 z-0",
+                "hover:after:translate-x-10 after:duration-200 after:transition-all relative", className)}>
+        {children}
+    </Link>
+  )
+}
+
+export default LinkTag
