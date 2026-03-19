392cb38cbe1257f215d5760bc346f2af2a4b0829
LakshrajChauhan13 <lakshrajchauhan13@gmail.com>
2026-03-19 19:37:16 +0530

    feat(fe): reStyled MessageBox UI and added copy button

--- CHAT-APP-FE/src/components/MessageBox.tsx
+++ CHAT-APP-FE/src/components/MessageBox.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/ui/popover"
 import { useState } from "react";
 import type { messageArrayInterface } from "./pages/ChatRoomPage";
+import { toast } from "sonner";
 
 interface MessageInterface {
     message: messageArrayInterface;
@@ -22,7 +23,7 @@ const MessageBox = ({message, userId, key, id, deleteMessage}: MessageInterface)
     }
 
   return (
-      <div className=" flex flex-col gap-1.5 shrink-0  w-full ">
+      <div className=" flex flex-col items-start gap-1.5 shrink-0  w-full ">
         <ChatMessage key={key} message={message} userId={userId} deleteMessage={onDelete} messageId={id}  
         className3="flex" 
         className2={`ml-1 rounded-tr-lg  `} 
@@ -63,30 +64,50 @@ const ChatMessage = ({className1, className2, className3, message, userId, delet
         setIsVisible(c => !c);    
     }
 
-    const isDeleted = message.message === 'This message has been deleted.';
+    function copyToClipBoard(){
+        navigator.clipboard.writeText(message.message);
+        toast.success('Copied to clipboard')
+    }
+
+    const isDeleted = message.type === 'deleted';
+    const isSender = userId === 'You';
+    
     return (
-        <div className="flex flex-col gap-2 group">
-            <div className={cn( "flex items-center gap-1 " , className3)}>
-                <div className={cn("size-8 bg-red-500 rounded-full", className1)} />
-                <span className="text-xs px-1 py-1 rounded-lg shadow-aceternity-dark text-neutral-500"> 
+        <div className={cn("flex flex-col gap-1 group max-w-[80%]", isSender ? "items-end" : "items-start")} >
+
+            {/* Header / Avatar Row */}
+            <div className={cn("flex items-center gap-1", className3)}>
+                <div className={cn("size-8 shrink-0 rounded-full", className1)} />
+                <span className="text-xs px-1.5 py-1 rounded-lg shadow-aceternity-dark text-neutral-500 whitespace-nowrap"> 
                     {userId}  
                 </span>
+                
+                {/* Popover */}
                 <Popover>
                     <PopoverTrigger asChild>
-                        <button onClick={visible} className={`group-hover:visible invisible ${isVisible ? 'visible' : ''} cursor-pointer ${isDeleted && 'hidden'}`}>
-                            {userId === "You" && <Vertical_ellipsisIcon /> }      
+                        <button onClick={visible} className={`cursor-pointer ${isDeleted && 'hidden'}`}>
+                            {isSender && <Vertical_ellipsisIcon /> }      
                         </button>
                     </PopoverTrigger>
-                    <PopoverContent onClick={deleteMessage} align="end" className="w-20 h-10">
-                        Delete
+                    <PopoverContent align="end" className="w-auto h-10 ">
+                        <span className="flex gap-2 items-center justify-center font-sans-flex">
+                            <button onClick={deleteMessage} className="active:text-red-700 text-sm hover:underline transition-all underline-offset-3 cursor-pointer">Delete</button>
+                            <h3 className="text-neutral-500/50 cursor-default text-xl"> | </h3>
+                            <button onClick={copyToClipBoard} className="active:text-neutral-400 font-thin text-sm  hover:underline transition-all text-neutral-200 underline-offset-3 cursor-pointer"> Copy </button>
+                        </span>
                     </PopoverContent>
                 </Popover>
             </div>
-            <div className={cn(" min-h-8 max-h-full max-w-120 wrap-break-word mr-1 text-base font-mono inset-shadow-aceternity-dark shadow-aceternity-dark p-2 bg-amber-950/90 text-neutral-50 rounded-b-lg",
-                `${message.type === 'deleted' ? "italic bg-neutral-700 opacity-50" : 'bg-neutral-950'}`,
-                className2)}>
-                {message.message}
+
+            <div className={cn("flex w-full", isSender ? "justify-end pr-9" : "justify-start pl-9")}>
+                <div className={cn(
+                    "inline-block w-fit max-w-full break-words text-base font-mono inset-shadow-aceternity-dark shadow-aceternity-dark px-3 py-2 text-neutral-50 rounded-b-xl",
+                    isDeleted ? "italic bg-neutral-700 opacity-50" : "bg-neutral-900",
+                    className2
+                )}>
+                    {message.message}
+                </div>
             </div>
         </div>
     )
-}   
+}
